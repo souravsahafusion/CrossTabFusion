9a0260ab40c193af92e88777b05d8cc82833f83f
souravsahafusion <sourav.saha@fusioncharts.com>
2016-08-17 13:12:40 -0700

    gencolor file created

--- js/chartCategory/CrossTab/crossTab.js
+++ js/chartCategory/CrossTab/crossTab.js
@@ -13,9 +13,6 @@
         this.sepSVG = '';
         this.productLen = productLen;
 
-
-
-
     }
    
     
@@ -36,35 +33,7 @@
     
    
     
-   CrossTab.prototype.genColor = function(ratio){
-    var color1,
-        color2;
-       if(ratio > 0){
-          color1 = jsonData.minProfitColor;
-          color2 = jsonData.maxProfitColor; 
-       }else{
-          color1 = jsonData.minLossColor;
-          color2 = jsonData.maxLossColor; 
-          //console.log(ratio);
-          ratio = ratio * -1;
-       }
-   color2 = color2.slice(1,color2.length);
-   color1 = color1.slice(1,color1.length);
-   
-
-   var hex = function(x) {
-       x = x.toString(16);
-       return (x.length == 1) ? '0' + x : x;
-   };
-
-   var r = Math.ceil(parseInt(color1.substring(0,2), 16) * ratio + parseInt(color2.substring(0,2), 16) * (1-ratio));
-   var g = Math.ceil(parseInt(color1.substring(2,4), 16) * ratio + parseInt(color2.substring(2,4), 16) * (1-ratio));
-   var b = Math.ceil(parseInt(color1.substring(4,6), 16) * ratio + parseInt(color2.substring(4,6), 16) * (1-ratio));
-
-   var middle = hex(r) + hex(g) + hex(b);
-   return "#"+middle;
-};
-
+  
     CrossTab.prototype.plotColumnChart = function() {
         
         var plot = new PlotGraph();
@@ -93,8 +62,8 @@
                 //var xPointPlot = 0/*this.lowLimitXAxis + (widthEachChart / this.noofXTips) * (i)*/;
                 var valueProfit = this.instance.productIns[this.ChartIndex].sop[i];
                 var ratio = valueProfit / value;
-                
-                var styleColor = this.genColor(ratio);
+                var generateColor = new GenColor();
+                var styleColor = generateColor.genColor(ratio);
                 console.log(ratio+ 'ratio');
                 //storeAncorPointsX[i] = Math.floor(xPointPlot);
                 var x = 0/*xPointPlot - widthEachChart * scaleColChartFactor*/;
