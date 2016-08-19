f4c82ce73be0bbe2c7a204c7d9a07823e8baa513
Sourav Saha <sourav.saha@fusioncharts.com>
2016-08-19 12:12:54 +0530

    gulp file added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,39 @@
-/* File: gulpfile.js */
-
-// grab our gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat'),
+    iife = require("gulp-iife"),
+    htmlreplace = require('gulp-html-replace');
+function createErrorHandler(name) {
+    return function (err) {
+      console.error('Error from ' + name + ' in compress task', err.toString());
+    };
+}
 
-// create a default task and just log a message
 gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+    gulp.src('./js/**/*')
+        .pipe(concat('chart.min.js'))
+        //.pipe(iife())
+        .pipe(uglify())
+        .on('error', createErrorHandler('uglify'))
+        .pipe(gulp.dest('./build/js/'));
+   
+    gulp.src('./json/*.json')
+        .pipe(gulp.dest('./build/json/'));
+   
+    gulp.src('./css/*.css')
+        .pipe(concat('chart.min.css'))
+        .pipe(iife())
+        .pipe(gulp.dest('./build/css/'));  
+   
+    gulp.src('index.html')
+        .pipe(htmlreplace({
+            'js': 'js/chart.min.js'
+        }))
+        .pipe(gulp.dest('./build/'));   
 });
+
+
+
+
+
